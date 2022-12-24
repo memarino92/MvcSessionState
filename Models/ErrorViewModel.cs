18563bb41983ba0704f6fadc14b0f136eb8dfa36
Michael <memarino92@gmail.com>
2022-12-24 15:33:15 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcSessionState.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
